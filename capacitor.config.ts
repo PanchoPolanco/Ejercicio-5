4a3807a4fa80cd09420886f1cb087404328e8f54
Pancho Polanco <cristian.pancho.cami@gmail.com>
2023-10-18 13:45:24 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ernestoverdugo.guia5',
+  appName: 'guia5',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
